ac2a7e935c6adb43428c590a4df7bede4fc94dd6
ms-O <kristin.obermyer@villanova.edu>
2020-03-24 14:20:32 -0400

    Added basic Maven project for Jenkins demo

---
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -0,0 +1,11 @@
+package edu.studio.sample;
+
+public class ConsoleApp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Jenkins");
+        
+    }
+
+}
---
+++ src/test/java/edu/studio/sample/SampleTest.java
@@ -0,0 +1,19 @@
+package edu.studio.sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+
+}
